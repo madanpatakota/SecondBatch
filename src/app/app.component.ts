11609ebbab9e3855a2fc856deaf1faf9c2682551
MadanMohanReddy <madan.patakota@hotmail.com>
2021-03-07 17:49:08 +0530

    Introduction,ComponentBasics,BindingBasics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,92 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-  title = 'TestApp';
+
+  //title = "AngularApp";
+
+  IsAlfredsCustomerAvailble = true;
+  customerName = "Alfreds"  // coming from DB
+  IsDisabled = false;
+
+
+  Adress = "bangolore";
+
+  customerBGColor = "yellow";
+
+  IsCustomerBGClass = true;
+
+  constructor() {
+    setTimeout(() => {
+      this.inputtype = "checkbox";
+      this.IsDisabled = true;
+      this.customerBGColor = "blue";
+    }, 10000);
+
+    if (this.customerName != "Alfreds") {
+      this.IsAlfredsCustomerAvailble = false;
+    }
+    if (this.Adress != "bangolore") {
+      this.customerBGColor = "brown";
+    }
+
+    if (this.Adress != "bangolore") {
+      this.IsCustomerBGClass = false;
+    }
+
+  }
+
+
+  inputtype = "textbox";
+
+  getCustomerName() {
+    return "Berglunds snabbköp";
+    //alert("Berglunds snabbköp");
+  }
+
+
+  getContactName() {
+    //return "Berglunds snabbköp";
+    alert("Christina Berglund");
+  }
+
+
+
+  Adress1 = "Hyderabad"
+
+  evtChanges($event) {
+    //console.log($event);
+    //console.log($event.target.value);
+    this.Adress = $event.target.value;
+  }
+
+
+
+  companyNames =
+    ["Berglunds snabbköp",
+      "Blauer See Delikatessen",
+      "Blondesddsl",
+      "Bólido Comidas preparadas"];
+
+
+  Customers =
+    [
+      { CustomerID: "BERGS", CompanyName: "Berglunds snabbköp" },
+      { CustomerID: "BLAUS", CompanyName: "Blauer See Delikatessen" },
+      { CustomerID: "BLONP", CompanyName: "Blondesddsl père et fils" },
+      { CustomerID: "BOLID", CompanyName: "Bólido Comidas preparadas" }
+    ]
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
