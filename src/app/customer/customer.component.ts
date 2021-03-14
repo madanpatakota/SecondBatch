897d0d9f28565c732d4af9809faf90d8c874f598
MadanMohanReddy <madan.patakota@hotmail.com>
2021-03-14 18:13:05 +0530

    Added LocalReferece , @input ,event emitter , @output , ngContent , @contentChild

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,16 +1,93 @@
 
 
-import { Component} from '@angular/core';
+import { Component, ElementRef, Input, ViewChild, EventEmitter, Output, ContentChild }
+    from '@angular/core';
 
 
 @Component({
-    selector : "app-customer",
-    templateUrl : "./customer.component.html",
-    styleUrls : ["./customer.component.css" , "./customerapp.component.css"]
+    selector: "app-customer",
+    templateUrl: "./customer.component.html",
+    styleUrls: ["./customer.component.css", "./customerapp.component.css"]
 })
 
 
-export class AppCustomer{
+export class AppCustomer {
+
+
+    //  x:boolean = 1;
+
+
+
+    // 1. I want to inputting|receive|fetch the records from out side.
+    // 2. By default in customer applicable only in the Appcustomer.
+
+    @Input() inCustomer: any;
+    @Input() inCID: string;
+    @Input() inCompName: string;
+    @Input() inContName: string;
+    //@Input() inCityName :string;
+
+    AddCustomer(CustomerID: HTMLInputElement,
+        CompanyName: HTMLInputElement,
+        ContactName: HTMLInputElement) {
+        console.log("CustomerID ", CustomerID.value);
+        console.log("CustomerName ", CompanyName.value);
+        console.log("ContactName ", ContactName.value);
+    }
+
+    customer = {
+        CustomerID: 'ANTON',
+        CompanyName: 'Antonio Moreno Taquería',
+        ContactName: 'Antonio Moreno'
+    }
+
+
+
+
+
+    @ViewChild('CustomerID') vCustomerID: ElementRef;  // select 
+    @ViewChild('CompanyName') vCompanyName: ElementRef;
+    @ViewChild('ContactName') vContactName: ElementRef;
+
+
+    //x = ""
+    @ContentChild('cusCityName') cCityName: ElementRef;
+
+    // something : HTMLInputElement
+
+    // @Input -> i am taking the Data;
+    // @output -> i am giving the data
+
+    @Output() customerEmitter =
+        new EventEmitter<{
+            CustomerID, CompanyName, ContactName,
+            CityName
+        }>();
+
+
+    AddOneCustomer() {
+        debugger;  // pause the compiler
+        console.log("----start-----")
+        console.log(this.vCustomerID.nativeElement.value);
+        console.log(this.vCompanyName.nativeElement.value);
+        console.log(this.vContactName.nativeElement.value);
+        console.log("cityname", this.cCityName.nativeElement.value);
+
+        this.customerEmitter.emit({
+            CustomerID: this.vCustomerID.nativeElement.value,
+            CompanyName: this.vCompanyName.nativeElement.value,
+            ContactName: this.vContactName.nativeElement.value,
+            CityName: this.cCityName.nativeElement.value
+        })
+    }
+
+
+    ClearCustomer() {
+        this.vCustomerID.nativeElement.value = "";
+        this.vCompanyName.nativeElement.value = "";
+        this.vContactName.nativeElement.value = "";
+    }
+
 
 }
 
